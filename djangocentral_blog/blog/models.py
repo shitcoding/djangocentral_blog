2e2c4ff752220ee7fbec22e1114625c38fadcb5b
shitcoding <shitcoder@pm.me>
2022-04-28 12:20:01 +0300

    add comment model and form

--- djangocentral_blog/blog/models.py
+++ djangocentral_blog/blog/models.py
@@ -43,4 +43,4 @@ class Comment(models.Model):
         ordering = ['created_on']
 
     def __str__(self):
-        return f'Comment {self.body} by {self.name}'
+        return f'Comment by {self.name}: "{self.body[:60]}"'
